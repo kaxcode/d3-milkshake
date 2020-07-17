89db26436319294deff99014891c339a5cb74a11
kenia <keniavalladarez@gmail.com>
2020-07-17 10:43:23 -0400

    Create the dimensions for our wrapper and bounds

--- 02-making-a-scatterplot/draft/draw-scatter.js
+++ 02-making-a-scatterplot/draft/draw-scatter.js
@@ -1,8 +1,32 @@
 async function drawScatter() {
+  // Access Data
   let dataset = await d3.json("./../../my_weather_data.json");
 
   const xAccessor = d => d.dewPoint;
   const yAccessor = d => d.humidity;
 
+  // Create Chart Dimensions
+  const width = d3.min([
+    window.innerWidth * 0.9,
+    window.innerHeight * 0.9,
+  ])
+
+  let dimensions = {
+    width: width,
+    height: width,
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  }
+
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.left
+    - dimensions.margin.right;
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom;
 }
 drawScatter()
