ba4b2ee83565fde339ec779773e9c7ab196d8019
kenia <keniavalladarez@gmail.com>
2020-07-18 20:33:42 -0400

    Create Dimensions for histrogram

--- 03-making-a-bar-chart/draft/draw-bars.js
+++ 03-making-a-bar-chart/draft/draw-bars.js
@@ -3,5 +3,24 @@ async function drawBars() {
   const dataset = await d3.json("./../../my_weather_data.json");
   const metricAccessor = d => d.humidity;
 
+  // Create Dimensions
+  const width = 600;
+  let dimensions = {
+    width: width,
+    height: width * 0.6,
+    margin: {
+      top: 30,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    },
+  }
+
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.left
+    - dimensions.margin.right;
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom;
 }
 drawBars()
